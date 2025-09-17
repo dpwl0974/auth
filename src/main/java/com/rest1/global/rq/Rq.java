757a2ebe65587fa4caabaf1c54a10a0d0504293a
dpwl0974 <dpwl0974@gmail.com>
2025-09-17 13:54:04 +0900

    refactor: rq scope 빼기

--- src/main/java/com/rest1/global/rq/Rq.java
+++ src/main/java/com/rest1/global/rq/Rq.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 @Component
-@RequestScope // 요청마다 생성 후 사라짐 -> 즉, 매번 달라짐
 @RequiredArgsConstructor
 public class Rq {
 
