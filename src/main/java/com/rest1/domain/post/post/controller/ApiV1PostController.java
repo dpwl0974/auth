0a2225942d03a0ef6cc09b52ffb41d3e8584558e
dpwl0974 <dpwl0974@gmail.com>
2025-09-18 15:20:08 +0900

    Open Api 문서에서 @SecurityScheme를 추가하여 문서 자체에 로그인 기능 추가. 헤더 방식으로 작동

--- src/main/java/com/rest1/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/rest1/domain/post/post/controller/ApiV1PostController.java
@@ -9,6 +9,7 @@ import com.rest1.global.exception.ServiceException;
 import com.rest1.global.rq.Rq;
 import com.rest1.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -23,6 +24,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts")
 @Tag(name = "ApiV1PostController", description = "글 API")
+@SecurityRequirement(name = "bearerAuth")
 public class ApiV1PostController {
 
     private final PostService postService;
