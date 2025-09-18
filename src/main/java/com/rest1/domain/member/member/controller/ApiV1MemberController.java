df196992fc773c0e4d78dcb7e1a12a5717f584f0
dpwl0974 <dpwl0974@gmail.com>
2025-09-18 12:22:04 +0900

    refactor: 로그인 시 쿠키 생성

--- src/main/java/com/rest1/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/rest1/domain/member/member/controller/ApiV1MemberController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Singular;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/members")
@@ -22,68 +23,83 @@ public class ApiV1MemberController {
     private final Rq rq;
 
     record JoinReqBody(
-      @NotBlank
-      @Size(min = 2, max = 30)
-      String username,
-      @NotBlank
-      @Size(min = 2, max = 30)
-      String password,
-      @NotBlank
-      @Size(min = 2, max = 30)
-      String nickname
-    ){}
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String username,
+
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String password,
+
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String nickname
+    ) {
+    }
 
     record JoinResBody(
-      MemberDto memberDto
-    ) {}
+            MemberDto memberDto
+    ) {
+    }
 
     @PostMapping("/join")
     public RsData<MemberDto> join(
             @RequestBody @Valid JoinReqBody reqBody
-            ){
-
+    ) {
         Member member = memberService.join(reqBody.username, reqBody.password, reqBody.nickname);
 
         return new RsData(
                 "201-1",
                 "회원가입이 완료되었습니다. %s님 환영합니다.".formatted(reqBody.nickname),
-                new JoinResBody(new MemberDto(member))
-
+                new JoinResBody(
+                        new MemberDto(member)
+                )
         );
     }
+
 
     record LoginReqBody(
             @NotBlank
             @Size(min = 2, max = 30)
             String username,
+
             @NotBlank
             @Size(min = 2, max = 30)
             String password
-    ){}
+    ) {
+    }
 
     record LoginResBody(
             MemberDto memberDto,
-            String apiKey // dto에 넣으면 데이터 주고받을 때도 쓰임 -> 비효율적 / 로그인시에만
-    ){}
+            String apiKey
+    ) {
+    }
 
     @PostMapping("/login")
     public RsData<MemberDto> login(
             @RequestBody @Valid LoginReqBody reqBody
-    ){
+    ) {
+
         Member member = memberService.findByUsername(reqBody.username).orElseThrow(
-                () -> new ServiceException("401-1","존재하지 앟는 아이디입니다." )
+                () -> new ServiceException("401-1", "존재하지 않는 아이디입니다.")
         );
 
-        if(!member.getPassword().equals(reqBody.password)){
+        if (!member.getPassword().equals(reqBody.password)) {
             throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
         }
+
+        rq.addCookie("apiKey", member.getApiKey());
 
         return new RsData(
                 "200-1",
                 "%s님 환영합니다.".formatted(reqBody.username),
-                new LoginResBody(new MemberDto(member), member.getApiKey())
+                new LoginResBody(
+                        new MemberDto(member),
+                        member.getApiKey()
+                )
         );
     }
+
 
     record MeResBody(
             MemberDto memberDto
