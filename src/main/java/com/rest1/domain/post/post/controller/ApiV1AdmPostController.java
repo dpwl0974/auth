552bdf75c46def0b78b63526f57ae61d09c90daa
dpwl0974 <dpwl0974@gmail.com>
2025-09-22 14:21:15 +0900

    feat: 글 통계 기능 API 구현

--- src/main/java/com/rest1/domain/post/post/controller/ApiV1AdmPostController.java
+++ src/main/java/com/rest1/domain/post/post/controller/ApiV1AdmPostController.java
@@ -1,31 +1,38 @@
-package com.rest1.domain.member.member.controller;
+package com.rest1.domain.post.post.controller;
 
-import com.rest1.domain.member.member.dto.MemberWithUsernameDto;
+
 import com.rest1.domain.member.member.entity.Member;
-import com.rest1.domain.member.member.service.MemberService;
+import com.rest1.domain.post.post.service.PostService;
 import com.rest1.global.exception.ServiceException;
 import com.rest1.global.rq.Rq;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/adm/members")
-public class ApiV1AdmMemberController {
+@RequestMapping("/api/v1/adm/posts")
+@Tag(name = "ApiV1AdmPostController", description = "관리자용 글 API")
+@SecurityRequirement(name = "bearerAuth")
+public class ApiV1AdmPostController {
 
-    private final MemberService memberService;
+    private final PostService postService;
     private final Rq rq;
 
-    @GetMapping
+    record CountResBody(
+            long totalCount
+    ) {
+    }
+
+    @GetMapping("/count")
     @Transactional(readOnly = true)
-    @Operation(summary = "회원 다건 조회")
-    public List<MemberWithUsernameDto> getItems() {
+    @Operation(summary = "글 개수 조회")
+    public CountResBody count() {
 
         Member actor = rq.getActor();
 
@@ -33,10 +40,7 @@ public class ApiV1AdmMemberController {
             throw new ServiceException("403-1", "권한이 없습니다");
         }
 
-        return memberService.findAll().stream()
-                .map(MemberWithUsernameDto::new)
-                .toList();
-
+        return new CountResBody(postService.count());
     }
 
 }
